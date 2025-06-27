1f06793fa04ff60eee9b005a86835a5c72331448
RayyanVirani <rayyanvirani07@gmail.com>
2025-06-27 15:41:56 +0500

    Fully Working Calculator App

--- Calculator.py
+++ Calculator.py
@@ -1,38 +1,53 @@
-print("This is a calculator!")
+def caculator():
 
-def addition(num1,num2):
-    result = num1 + num2
-    print(result)
+    print("This is a calculator!")
 
-def subtraction(num1,num2):
-    result = num1 - num2
-    print(result)
+    def addition(num1,num2):
+        result = num1 + num2
+        print(result)
 
-def multiplication(num1,num2):
-    result = num1 * num2
-    print(result)
+    def subtraction(num1,num2):
+        result = num1 - num2
+        print(result)
 
-def division(num1 , num2):
-    result = num1 / num2
-    print(result)
+    def multiplication(num1,num2):
+        result = num1 * num2
+        print(result)
 
-num1 = int(input("Enter number 1: "))
-num2 = int(input("Enter number 2: "))
+    def division(num1 , num2):
+        result = num1 / num2
+        print(result)
 
-print("Press 1 for Addition")
-print("press 2 for Subtraction")
-print("Press 3 for Multiplication")
-print("Press 4 for Division")
+    num1 = int(input("Enter number 1: "))
+    num2 = int(input("Enter number 2: "))
 
-operation = int(input("Enter number: "))
+    print("Press 1 for Addition")
+    print("press 2 for Subtraction")
+    print("Press 3 for Multiplication")
+    print("Press 4 for Division")
 
-if operation == 1:
-    addition(num1,num2)
-elif operation == 2:
-    subtraction(num1,num2)
-elif operation == 3:
-    multiplication(num1,num2)
-elif operation == 4:
-    division(num1 , num2)
-else:
-    print("Wrong Operation number")
+    operation = int(input("Enter number: "))
+
+    if operation == 1:
+        addition(num1,num2)
+    elif operation == 2:
+        subtraction(num1,num2)
+    elif operation == 3:
+        multiplication(num1,num2)
+    elif operation == 4:
+        division(num1 , num2)
+    else:
+        print("Wrong Operation number")
+
+keep_running = 1
+caculator()
+
+while keep_running == 1:
+    print("Press 1 if you want to keep the program running")
+    print("Press 2 if you want to close the program")
+    ask = int(input("Do you want to keep the program running? "))
+    if ask == 2:
+        print("Thanks for using the caculator")
+        keep_running = 2
+    if ask == 1:    
+        caculator()
